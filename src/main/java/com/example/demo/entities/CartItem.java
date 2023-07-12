5c0f5810aa91da1333290b4f6351bf8a8c733a3d
Jacil <jper349@wgu.edu>
2023-07-12 12:30:20 -0700

    spring annotations 1

--- src/main/java/com/example/demo/entities/CartItem.java
+++ src/main/java/com/example/demo/entities/CartItem.java
@@ -1,20 +1,38 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.util.Date;
 import java.util.Set;
-
+@Entity
+@Table(name = "CART_ITEMS")
+@Data
 public class CartItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Cart_Item_ID")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "Vacation_ID")
     private Vacation vacation;
 
     private Set<Excursion> excursions;
 
+    @ManyToOne
+    @JoinColumn(name = "Cart_ID", nullable = false)
     private Cart cart;
 
+    @Column(name = "Create_Date")
+    @CreationTimestamp
     private Date createDate;
 
+    @Column(name = "Last_Update")
+    @UpdateTimestamp
     private Date lastUpdate;
 
     public CartItem() {
--- src/main/java/com/example/demo/entities/Vacation.java
+++ src/main/java/com/example/demo/entities/Vacation.java
@@ -1,23 +1,42 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 
+@Entity
+@Table(name = "VACATIONS")
+@Data
 public class Vacation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Vacation_ID")
     private Long id;
 
+    @Column(name = "Vacation_Title")
     private String vacationTitle;
 
+    @Column(name = "Description")
     private String description;
 
+    @Column(name = "Travel_Fare_Price")
     private BigDecimal travelPrice;
 
+    @Column(name = "Image_URL")
     private String imageUrl;
 
+    @CreationTimestamp
+    @Column(name = "Create_Date")
     private Date createDate;
 
+    @UpdateTimestamp
+    @Column(name = "Last_Update")
     private Date lastUpdate;
 
     private Set<Excursion> excursions;
