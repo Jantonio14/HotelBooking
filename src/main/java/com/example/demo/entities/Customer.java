4bf9655747abddcc3ce7830e3f448d42b32345c3
Jacil <jper349@wgu.edu>
2023-07-12 08:42:04 -0700

    Imported application properties from lab files

---
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -0,0 +1,30 @@
+package com.example.demo.entities;
+
+import java.util.Date;
+
+public class Customer {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String postalCode;
+
+    private String phone;
+
+    private Date createData;
+
+    private Date lastUpdate;
+
+    private Long division;
+
+    public Customer() {
+
+    }
+
+
+}
